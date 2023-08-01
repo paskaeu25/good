431e98a612171939cde33e383960996aa7c6c6be
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2023-08-01 13:51:30 +0300

    Added JS to add task to task list

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 const todoForm = document.querySelector("#todoForm");
 const taskInput = document.querySelector("#taskInput");
 const taskList = document.querySelector("#taskList");
+
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addTask();
+});
+
+function addTask() {
+  const taskText = taskInput.value.trim();
+  if (taskText === "") alert("Please enter a task!");
+
+  const li = document.createElement("li");
+  li.innerHTML = taskText;
+  taskList.appendChild(li);
+
+  taskInput.value = "";
+}
